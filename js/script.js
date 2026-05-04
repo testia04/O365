569a23e616412ffb807030e1caab4b871f3a1d56
testia04 <testia04000@gmail.com>
2026-05-04 13:22:14 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn");
+
+    if (btn) {
+        btn.addEventListener("click", () => {
+            alert("JavaScript OK !");
+        });
+    }
+});
